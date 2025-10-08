08f6f6cd8e91a10488029948f6332a88568e8443
puvanakopis <puvanakopis@gmail.com>
2025-10-08 19:50:05 +0530

    Add Nodemailer transporter configuration

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import userModel from '../models/userModel.js'
+import transporter from '../config/nodemailer.js'
 
 export const register = async (req, res) => {
     const { name, email, password } = req.body
@@ -30,6 +31,16 @@ export const register = async (req, res) => {
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
+
+        // Sending welcome email
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email,
+            subject: 'Welcome to My Web Site',
+            text: `Welcome to my web site. Your account has been created with email id ${email}`
+        }
+
+        await transporter.sendMail(mailOptions)
 
         return res.json({ success: true })
 
