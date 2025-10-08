af2a45374fe8d33e5d76ba96ec8a654f1d4425aa
puvanakopis <puvanakopis@gmail.com>
2025-10-08 14:17:22 +0530

    connected with database

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,20 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import cookieParser from 'cookie-parser'
+import connectDB from './config/monodb.js'
+
 
 const app = express()
+const port = process.env.PORT || 4000
+connectDB()
 
-const port = process.env.POT || 4000
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ credentials: true }))
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("API is working ...")
 })
 
