ec2035aad2de75d1fcef75a131c4f2caafc50a9a
puvanakopis <puvanakopis@gmail.com>
2025-10-08 23:13:12 +0530

    Added email verification logic

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -2,6 +2,8 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import userModel from '../models/userModel.js'
 import transporter from '../config/nodemailer.js'
+import pkg from 'body-parser';
+const { text } = pkg;
 
 export const register = async (req, res) => {
     const { name, email, password } = req.body
@@ -98,5 +100,76 @@ export const logout = async (req, res) => {
         return res.json({ success: true, message: 'Logged Out' })
     } catch (error) {
         return res.send({ success: false, message: error.message })
+    }
+}
+
+
+export const sentVeriFyOtp = async (req, res) => {
+    try {
+        const { userId } = req.body
+
+        const user = await userModel.findById(userId)
+
+        if (user.isAccountVerified) {
+            return res.json({ success: false, message: "Account already verified" })
+        }
+
+        const otp = String(Math.floor(100000 + Math.random() * 900000))
+
+        user.verifyOtp = otp
+        user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000
+
+        await user.save()
+
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: "Account Verification OTP",
+            text: `Your OTP is ${otp}. verify your account using this OTP`
+        }
+
+        await transporter.sendMail(mailOptions)
+
+        res.json({ success: true, message: "Verification OTP sent on Email" })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+
+
+export const verifyEmail = async (req, res) => {
+    const { userId, otp } = req.body
+
+    if (!userId || !otp) {
+        return res.json({ success: false, message: "Missin Details" })
+    }
+
+    try {
+        const user = await userModel.findById(userId)
+
+        if (!user) {
+            return res.json({ success: false, message: "User not found" })
+        }
+
+        if (user.verifyOtp === '' || user.verifyOtp !== otp) {
+            return res.json({ success: false, message: "Invalid OTP" })
+        }
+
+        if (user.verifyOtpExpireAt < Date.now()) {
+            return res.json({ success: false, message: "OTP Expired" })
+        }
+
+        user.isAccountVerified = true
+        user.verifyOtp = ''
+        user.verifyOtpExpireAt = 0
+
+        await user.save()
+        return res.json({ success: true, message: "Email verifyed successfully" })
+
+
+    } catch (error) {
+        return res.json({ success: false, message: error.message })
     }
 }
--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { register, login, logout } from '../controller/authController.js'
+import { register, login, logout, sentVeriFyOtp, verifyEmail } from '../controller/authController.js'
+import userAuth from '../middleware/userAuth.js'
 
 const authRouter = express.Router()
 
 authRouter.post('/register', register)
 authRouter.post('/login', login)
 authRouter.post('/logout', logout)
+authRouter.post('/send-verify-otp', userAuth, sentVeriFyOtp)
+authRouter.post('/verify-account', userAuth, verifyEmail)
 
 export default authRouter
