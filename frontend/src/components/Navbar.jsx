2c540b5d0ea41fe82d17bc020dd30d90a0c51d04
puvanakopis <puvanakopis@gmail.com>
2025-10-09 12:25:58 +0530

    created log in page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import { assets } from '../assets/assets.js'
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+
+    const navigate = useNavigate()
+
     return (
         <div className='w-full fixed top-0 left-0 z-50 backdrop-blur-md bg-white/70 flex justify-between items-center p-4 sm:p-6 sm:px-24'>
             {/* Logo */}
@@ -11,7 +15,7 @@ const Navbar = () => {
             />
 
             {/* Login Button */}
-            <button className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all duration-200 cursor-pointer'>
+            <button onClick={() => navigate('/login')} className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all duration-200 cursor-pointer'>
                 Login <img src={assets.arrow_icon} alt="" />
             </button>
         </div>
