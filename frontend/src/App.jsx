9a9b89696cdd32ebaff1863c7240026b7e2cb4ca
puvanakopis <puvanakopis@gmail.com>
2025-10-09 10:48:42 +0530

    Add Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import EmailVerify from './pages/EmailVerify'
+import ResetPassword from './pages/ResetPassword'
 
 const App = () => {
   return (
     <div className='app'>
-      <h1>React Login</h1>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/email-verify' element={<EmailVerify/>} />
+        <Route path='/reset-password' element={<ResetPassword/>} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { assets } from '../assets/assets.js'
+
+const Navbar = () => {
+    return (
+        <div className='w-full fixed top-0 left-0 z-50 backdrop-blur-md bg-white/70 flex justify-between items-center p-4 sm:p-6 sm:px-24'>
+            {/* Logo */}
+            <img
+                src={assets.logo}
+                alt="Logo"
+                className='w-28 sm:w-32 cursor-pointer'
+            />
+
+            {/* Login Button */}
+            <button className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all duration-200 cursor-pointer'>
+                Login <img src={assets.arrow_icon} alt="" />
+            </button>
+        </div>
+    )
+}
+
+export default Navbar
